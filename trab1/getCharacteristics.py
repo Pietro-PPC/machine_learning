517fb9afccf55c4796509132f0a29e800472c0be
PIETRO POLINARI CAVASSIN <ppc19@inf.ufpr.br>
2023-08-18 19:04:32 -0300

    add functions

--- trab1/getCharacteristics.py
+++ trab1/getCharacteristics.py
@@ -3,7 +3,7 @@ import numpy as np
 
 PTMAP = [
     [7,  0, 1],
-    [6, -1, 2],
+    [6,  8, 2],
     [5,  4, 3]
 ]
 
@@ -14,8 +14,9 @@ def incrementContourHistogram(contour, hist):
 
         map_i = curpt[0]-prevpt[0]
         map_j = curpt[1]-prevpt[1]
-        map_i = map_i // abs(map_i) + 1
-        map_j = map_j // abs(map_j) + 1
+        if map_i != 0: map_i = map_i // abs(map_i)
+        if map_j != 0: map_j = map_j // abs(map_j)
+        map_i += 1; map_j += 1
 
         hist[ PTMAP[map_i][map_j] ] += 1
 
@@ -23,25 +24,39 @@ def incrementContourHistogram(contour, hist):
     
     return hist
 
+def preprocess(img):
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.resize(img, NEW_SIZE)
+    _, thresh = cv.threshold(img, 200, 255, cv.THRESH_BINARY_INV)
+    return img
+
+def getCharacteristics(img):
+    contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    showContours(contours)
+
+    hist = [0]*8
+    for contour in contours:
+        hist = incrementContourHistogram(contour, hist)
+    return hist
+
+def showContours(contours):
+    blackimg = np.zeros(NEW_SIZE, dtype=np.uint8)
+    cv.drawContours(blackimg, contours, -1, 127)
+    cv.imshow("contours", blackimg)
+    cv.waitKey(0)
+    
+    
+
 DATA_DIR = "digits/data/"
 NEW_SIZE = (50,50)
+if __name__ == '__main__':
 
-imgFile = DATA_DIR + "cdf0361_07_13_0.jpg"
-cvImg = cv.imread(imgFile)
-assert cvImg is not None, "file could not be read, check with os.path.exists()"
+    imgFile = DATA_DIR + "cdf0361_07_13_0.jpg"
+    cvImg = cv.imread(imgFile)
+    assert cvImg is not None, "file could not be read, check with os.path.exists()"
 
-cvImg = cv.cvtColor(cvImg, cv.COLOR_BGR2GRAY)
-cvImg = cv.resize(cvImg, NEW_SIZE)
-_, thresh = cv.threshold(cvImg, 200, 255, cv.THRESH_BINARY_INV)
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # preprocess image
+    cvImg = preprocess(cvImg)
+    characteristics = getCharacteristics(cvImg)
 
-hist = [0]*8
-for contour in contours:
-    incrementContourHistogram(contour, hist)
-
-blackimg = np.zeros(NEW_SIZE, dtype=np.uint8)
-cv.drawContours(blackimg, contours, -1, 128)
-
-print(hist)
-cv.imshow("img", blackimg)
-cv.waitKey(0)
+    print(characteristics)
